d610ed599a6ce78f71de393ae3b871d4d8d1b838
Marcelino2226250001 <marcelino_2226250001@mhs.mdp.ac.id>
2025-06-23 23:32:20 +0700

    update vite.config.ts

--- client/inventarisbarang/vite.config.ts
+++ client/inventarisbarang/vite.config.ts
@@ -1,23 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vuetify from 'vite-plugin-vuetify'
 import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vuetify({
-      autoImport: true,
-    }),
-  ],
-  define: {
-    'process.env': {}
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      vuetify({
+        autoImport: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-})
+    define: {
+      'import.meta.env': env // ⬅️ ini yang penting!
+    },
+  };
+});
