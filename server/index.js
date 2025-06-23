f4de37c60947e5a1f9339cc6b35b22fbb1129756
Marcelino2226250001 <marcelino_2226250001@mhs.mdp.ac.id>
2025-06-23 21:59:27 +0700

    update domain vercel

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors({
     'http://localhost:8080', // Vue CLI default port  
     'http://localhost:3000', // alternative port
     'http://localhost:5183', // jika ada port lain
-    'https://vueproject-eight.vercel.app/' // tambahkan domain production Vercel
+    'https://vueproject-murex.vercel.app/' // tambahkan domain production Vercel
   ],
   credentials: true, // penting untuk session cookies
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
